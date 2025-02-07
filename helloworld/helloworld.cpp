bf2874aadf8684e35f80dd6287d9d2f506a43412
Marco Asus Laptop <marcovillegas.g@gmail.com>
2025-02-07 13:57:20 -0300

    First test in c++

---
+++ helloworld/helloworld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::cout << "Esto es una prueba del debugger" << std::endl;
+    std::cout << "Hola Mundo!" << std::endl;
+    return 0;
+}
